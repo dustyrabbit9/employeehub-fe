3e267e300edc4c201b81af32186ab7f11b6532d5
Imran Rasheed <imran.2001r@gmail.com>
2023-03-18 22:32:15 +0530

    Added theme.js with project color scheme

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+import { color } from "@mui/system";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#146C94",
+      light: "#19A7CE",
+    },
+    secondary: {
+      main: "#F6F1F1",
+      light: "#FFFFFF",
+    },
+    otherColor: {
+      main: "#146C94",
+    },
+  },
+});
